0ccfbe28fee78bc4774b340e58d62f5a55366237
urabexon <hiroki708.a48uk@gmail.com>
2024-08-30 19:13:27 +0900

    add

---
+++ level1/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 17:43:08 by urabex            #+#    #+#             */
+/*   Updated: 2024/08/30 17:49:53 by urabex           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putnbr(int num)
+{
+    char str[10] = "0123456789";
+
+    if (num > 9)
+        ft_putnbr(num / 10);
+    write(1, &str[num % 10], 1);
+}
+
+int main(void)
+{
+    int i = 1;
+
+    while (i < 100)
+    {
+        if (i % 15 == 0)
+            write(1, "fizzbuzz", 8);
+        else if (i % 3 == 0)
+            write(1, "fizz", 4);
+        else if (i % 5 == 0)
+            write(1, "buzz", 4);
+        else
+            ft_putnbr(i);
+        i++;
+        write(1, "\n", 1);
+    }
+    return (0);
+}
